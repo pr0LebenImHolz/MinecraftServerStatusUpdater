8f896b7224f038bafb0a5623ede6ec3f6f951de2
fivekWBassMachine <53333237+fivekWBassMachine@users.noreply.github.com>
2022-02-11 18:22:54 +0100

    hardcoded version in main class and mcmod.info (forge does not overwrite )

--- src/main/java/com/github/fivekwbassmachine/minecraftserverstatusupdater/ServerStatusUpdater.java
+++ src/main/java/com/github/fivekwbassmachine/minecraftserverstatusupdater/ServerStatusUpdater.java
@@ -23,7 +23,7 @@ import java.io.IOException;
 public class ServerStatusUpdater {
 
     public static final String MOD_ID = "serverstatusupdater";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String NAME = "ServerStatusUpdater";
 
     private static Logger logger;
